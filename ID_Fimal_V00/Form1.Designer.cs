05f0b1bb34eb94d95533e199c87b4d21912b1ceb
id <ismail.demir@emikon.com.tr>
2023-07-22 12:54:26 +0200

    First

---
+++ ID_Fimal_V00/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace ID_Fimal_V00
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CB_Ekle01 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.CB_Ekle03 = new System.Windows.Forms.ComboBox();
+            this.CB_Ekle02 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.B_Ekle = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CB_Ekle01
+            // 
+            this.CB_Ekle01.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CB_Ekle01.FormattingEnabled = true;
+            this.CB_Ekle01.Location = new System.Drawing.Point(6, 45);
+            this.CB_Ekle01.Name = "CB_Ekle01";
+            this.CB_Ekle01.Size = new System.Drawing.Size(170, 23);
+            this.CB_Ekle01.TabIndex = 0;
+            this.CB_Ekle01.SelectedIndexChanged += new System.EventHandler(this.CB_Ekle01_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(72, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "EKLE";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1178, 720);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.DataGridView1);
+            this.tabPage1.Controls.Add(this.TextBox1);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1170, 694);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(786, 29);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.Size = new System.Drawing.Size(350, 606);
+            this.DataGridView1.TabIndex = 3;
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Enabled = false;
+            this.TextBox1.Location = new System.Drawing.Point(786, 6);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(350, 20);
+            this.TextBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.B_Ekle);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.CB_Ekle03);
+            this.groupBox1.Controls.Add(this.CB_Ekle02);
+            this.groupBox1.Controls.Add(this.CB_Ekle01);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(755, 630);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "MALZEME EKLEME";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(674, 601);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(674, 572);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CB_Ekle03
+            // 
+            this.CB_Ekle03.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CB_Ekle03.FormattingEnabled = true;
+            this.CB_Ekle03.Location = new System.Drawing.Point(358, 45);
+            this.CB_Ekle03.Name = "CB_Ekle03";
+            this.CB_Ekle03.Size = new System.Drawing.Size(170, 23);
+            this.CB_Ekle03.TabIndex = 0;
+            this.CB_Ekle03.SelectedIndexChanged += new System.EventHandler(this.CB_Ekle01_SelectedIndexChanged);
+            // 
+            // CB_Ekle02
+            // 
+            this.CB_Ekle02.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CB_Ekle02.FormattingEnabled = true;
+            this.CB_Ekle02.Location = new System.Drawing.Point(182, 45);
+            this.CB_Ekle02.Name = "CB_Ekle02";
+            this.CB_Ekle02.Size = new System.Drawing.Size(170, 23);
+            this.CB_Ekle02.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(408, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "EKLE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(241, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "EKLE";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1170, 694);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 90);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(522, 415);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // B_Ekle
+            // 
+            this.B_Ekle.Location = new System.Drawing.Point(534, 45);
+            this.B_Ekle.Name = "B_Ekle";
+            this.B_Ekle.Size = new System.Drawing.Size(75, 23);
+            this.B_Ekle.TabIndex = 6;
+            this.B_Ekle.Text = "Ekle";
+            this.B_Ekle.UseVisualStyleBackColor = true;
+            this.B_Ekle.Click += new System.EventHandler(this.B_Ekle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1193, 735);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "ID --- MALZEME LİSTESİ HAZIRLAMA PLATFORMU";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CB_Ekle01;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView DataGridView1;
+        private System.Windows.Forms.ComboBox CB_Ekle03;
+        private System.Windows.Forms.ComboBox CB_Ekle02;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button B_Ekle;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ ID_Fimal_V00/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Data.OleDb;
+
+namespace ID_Fimal_V00
+{
+    public partial class Form1 : Form   
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CB_Ekle01.Text = "Please select";
+            TextBox1.Text = dbAdres;
+            DbOku();
+        }
+
+
+        #region Excel Değişkenleri
+        public void pintu(string s)
+        {
+            con = new OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0; " + "data source='" + s + " '; " + "Extended Properties=Excel 8.0;");
+        }
+        public OleDbConnection con;
+        public OleDbCommand com;
+        public DataSet ds;
+        public OleDbDataAdapter oledbda;
+        public DataTable dt;
+        public string str;
+        public string dbAdres = Application.StartupPath +  "\\db.xls";
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfiledialog1 = new OpenFileDialog();
+            openfiledialog1.ShowDialog();
+            openfiledialog1.Filter = "allfiles|*.xls";
+
+        }
+
+        public void DbOku()
+        {
+            pintu(dbAdres);
+            try
+            {
+                con.Open();
+                str = "select * from [sheet1$]";
+                com = new OleDbCommand(str, con);
+                ds = new DataSet();
+                oledbda = new OleDbDataAdapter(com);
+                oledbda.Fill(ds, "[sheet1$]");
+                con.Close();
+                DataGridView1.DataSource = ds;
+                DataGridView1.DataMember = "[sheet1$]";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            try
+            {
+                con.Open();
+                str = "select * from [sheet1$]";
+                com = new OleDbCommand(str, con);
+                oledbda = new OleDbDataAdapter(com);
+                ds = new DataSet();
+                oledbda.Fill(ds, "[sheet1$]");
+                con.Close();
+
+                dt = ds.Tables["[sheet1$]"];
+                int i = 0;
+                for (i = 0; i <= dt.Rows.Count - 1; i++)
+                {
+                    CB_Ekle01.Items.Add(dt.Rows[i].ItemArray[0]);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+     
+        }
+
+        private void CB_Ekle01_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            CB_Ekle02.Items.Clear();
+            CB_Ekle02.Text = "";
+            try
+            {
+                con.Open();
+                str = "select * from [sheet1$]";
+                com = new OleDbCommand(str, con);
+                oledbda = new OleDbDataAdapter(com);
+                ds = new DataSet();
+                oledbda.Fill(ds, "[sheet1$]");
+                con.Close();
+
+                dt = ds.Tables["[sheet1$]"];
+                int i = 0;
+                for (i = 0; i <= dt.Rows.Count - 1; i++)
+                {
+                    int j = 2;
+                    CB_Ekle02.Items.Add(dt.Rows[i][CB_Ekle01.SelectedIndex + 1]); //.ItemArray[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void B_Ekle_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += CB_Ekle01.Text + " - " + CB_Ekle02.Text + " - " + CB_Ekle03.Text + Environment.NewLine;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*     Excel.Application ExcelUygulama;
+                Excel.Workbook ExcelSayfa;
+                object Missing = System.Reflection.Missing.Value;
+                Excel.Range ExcelRange;
+
+                int rowCnt = 0;
+                int columnCnt = 0;
+
+                string s_DosyaAdi = "";
+                string s_Veri = "";
+        */
+
+    }
+}
